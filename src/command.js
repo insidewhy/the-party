50f026fd22f70394981ba3cce0bab0ad78032e1d
James Pike <jpike@chilon.net>
2013-09-26 21:34:38 +0100

    Generate source maps when bootstrapping.

--- src/command.js
+++ src/command.js
@@ -7,7 +7,7 @@ exports.main = function(args) {
     .option('-d, --dump', 'Dump ASTs')
     .option(' --dump-locs', 'Include locs in dump')
     .option(' --dump-sources', 'Dump source ASTs rather than compiled ASTs')
-    .option('-s, --source-maps', 'Generate source maps')
+    .option('-m, --source-maps', 'Generate source maps')
     .parse(args)
 
   theparty.compile(opts.args, opts)
