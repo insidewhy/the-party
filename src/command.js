a36ee9bc2929ab17aa03013d0bdeffaa9b4d94de
James Pike <jpike@chilon.net>
2013-09-28 14:25:38 +0100

    Move options to their own module.

--- src/command.js
+++ src/command.js
@@ -1,14 +1,8 @@
 module theparty from './the-party'
-module opts from 'commander'
+module opts from './opts'
 
 exports.main = args => {
-  opts
-    .option('-o, --output <dir>', 'Output compiled JavaScript files to <dir>')
-    .option('-d, --dump', 'Dump ASTs')
-    .option('-L, --dump-locs', 'Include locs in dump')
-    .option('-S, --dump-sources', 'Dump source ASTs rather than compiled ASTs')
-    .option('-m, --source-maps', 'Generate source maps')
-    .parse(args)
+  opts.parse(args)
 
   var ret = theparty.compile(opts.args, opts)
   if (opts.dump || opts.dumpSources) {
---
+++ src/opts.js
@@ -0,0 +1,8 @@
+module opts from 'commander'
+
+module.exports = opts
+  .option('-o, --output <dir>', 'Output compiled JavaScript files to <dir>')
+  .option('-d, --dump', 'Dump ASTs')
+  .option('-L, --dump-locs', 'Include locs in dump')
+  .option('-S, --dump-sources', 'Dump source ASTs rather than compiled ASTs')
+  .option('-m, --source-maps', 'Generate source maps')
