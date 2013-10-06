f29644dc197d59122124c1f41e757630c5eb5f80
James Pike <jpike@chilon.net>
2013-10-06 01:16:41 +0100

    Move opts module back into command.

--- src/command.js
+++ src/command.js
@@ -1,5 +1,13 @@
 import compile from './the-party'
-module opts from './opts'
+module opts from 'commander'
+
+opts
+  .option('-c, --compile', 'Compile output files into same directory as sources')
+  .option('-o, --output <dir>', 'Output compiled JavaScript files to <dir>')
+  .option('-d, --dump', 'Dump ASTs')
+  .option('-L, --dump-locs', 'Include locs in dump')
+  .option('-S, --dump-sources', 'Dump source ASTs rather than compiled ASTs')
+  .option('-m, --source-maps', 'Generate source maps')
 
 export function main(args) {
   opts.parse(args)
--- src/opts.js
+++
@@ -1,9 +0,0 @@
-module opts from 'commander'
-
-module.exports = opts
-  .option('-c, --compile', 'Compile output files into same directory as sources')
-  .option('-o, --output <dir>', 'Output compiled JavaScript files to <dir>')
-  .option('-d, --dump', 'Dump ASTs')
-  .option('-L, --dump-locs', 'Include locs in dump')
-  .option('-S, --dump-sources', 'Dump source ASTs rather than compiled ASTs')
-  .option('-m, --source-maps', 'Generate source maps')
